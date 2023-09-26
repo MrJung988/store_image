f43f9d6a5ffbbd27c2ef2d53e4a2fff4488add66
Samir Karki <karkisamir99@gmail.com>
2023-09-26 23:54:55 +0545

    Add multiple image functionality

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Image;
+use App\Models\MultipleImage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -37,5 +38,44 @@ class ImageController extends Controller
             'message' => 'Image uploaded successfully',
             'success' => true,
         ], 200);
+    }
+
+    public function storeMultipleImage(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'images.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Validation errors',
+                    'error' => $validator->errors(),
+                    'success' => false,
+                ], 400);
+            }
+
+            $uploadedImages = [];
+
+            foreach ($request->file('images') as $image) {
+                $imageNewName = time() . '_' . uniqid() . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('multiple-images'), $imageNewName);
+
+                MultipleImage::create(['images' => $imageNewName]);
+
+                $uploadedImages[] = $imageNewName;
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Images uploaded successfully',
+                'images' => $uploadedImages,
+            ]);
+        } catch (\Throwable $th) {
+            return response([
+                'success' => false,
+                'message' => 'Something went wrong'
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('image', [ImageController::class, 'storeImage']);
+Route::post('multiple-image', [ImageController::class, 'storeMultipleImage']);
